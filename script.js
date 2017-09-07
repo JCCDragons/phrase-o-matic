182b5751e2654e74fc422626375d5ee5037d7350
JCCDragons <janice.c.chung@gmail.com>
2017-09-07 22:27:16 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function generate() {
+  // word lists
+  var nounList = ["cat", "dog", "trombone", "apple", "duck", "robot", "solar flare", "dodecahedron", "diatom", "sausage"];
+  var verbList = ["ran on", "ninja-kicked", "karate-chopped", "];
+  var nounList2 = ["tree", "fence", "book", "concrete", "table"];
+  
+  // calculates how long each word list is
+  var noun = nounList.length;
+  var verb = verbList.length;
+  var noun2 = nounList2.length;
+  
+  // the sentence to print out
+  var stringy = "The " + noun + " " + verb + " the " + noun2 + "."
+  
+  // sets the inner HTML of the quote box to the sentence just created
   document.getElementById("quotebox").innerHTML = "Hi!"
 }
-
-// var quotebox = document.getElementById("quotebox");
-
-// quotebox.onclick = function() {
-//   };
-
-// quotebox.addEventListener("click", myScript);
