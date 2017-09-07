074d2349bfa857eb66838a0f824eb8069ef4f71e
JCCDragons <janice.c.chung@gmail.com>
2017-09-07 22:09:09 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function generate() {
   document.getElementById("quotebox").innerHTML = "Hi!"
 }
+
+var quotebox = document.getElementById("quotebox");
+
+quotebox.onclick = function() {
+  };
+
+quotebox.addEventListener("click", myScript);
